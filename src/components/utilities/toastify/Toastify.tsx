c3dcdadd4cf2099a995164b86e2776a6b0807c59
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-14 15:54:46 +0300

    Update Toastify.tsx
    Moved error toastify to the file to facilitate reuse

--- src/components/utilities/toastify/Toastify.tsx
+++ src/components/utilities/toastify/Toastify.tsx
@@ -13,3 +13,19 @@ export const successNotification = async (message: string) => {
     theme: "light",
   });
 };
+
+
+export const errorNotification = async (message: string) => {
+  await toast.error(message, {
+    position: "top-center",
+    toastId: 5483,
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: false,
+    draggable: false,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
