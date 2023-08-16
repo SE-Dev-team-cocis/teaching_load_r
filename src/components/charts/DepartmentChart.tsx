541c60f18f1f6727a456c60753b27f4f32a97704
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-16 04:45:16 +0300

    Update DepartmentChart.tsx
    Fixed below chart colors

--- src/components/charts/DepartmentChart.tsx
+++ src/components/charts/DepartmentChart.tsx
@@ -7,16 +7,15 @@ const DepartmentChart = ({ data }: any) => {
 
       const chartData = {
         labels: ["Minimum Load", "Under Load", "Extra Load"],
-
         datasets: [
           {
-            // label: "Department Summary",
+            label: "Department Summary",
             data: [...data],
             fill: true,
             backgroundColor: [
-              "rgba(62, 110, 62, 0.9)",
-              "rgba(233, 82, 82, 0.7)",
-              "rgba(248, 248, 25, 0.97)",
+              "#ffff33",
+              "#ff0000",
+              "#2d862d",
             ],
           },
         ],
