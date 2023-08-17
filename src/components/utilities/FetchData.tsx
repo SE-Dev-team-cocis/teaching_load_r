b78dfc224ccb99a502d1c79ccfd2b978e51e0f83
Jacobson <romeojacobson@gmail.com>
2023-08-17 22:29:37 +0300

    imported setter function for lecturers

--- src/components/utilities/FetchData.tsx
+++ src/components/utilities/FetchData.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { Course, fetchCourses, fetchLecturers, fetchLoad } from "../../zustand/api/apis";
+import useNewLoadStore21 from "../../zustand/newLoadStore2";
 
 export function fetchData() {
+   const setLecturers = useNewLoadStore21(state => state.setLecturers) 
   // Fetching all load
   const { data: loads, isSuccess: loadedLoads } = useQuery({
     queryKey: ["load"],
     queryFn: fetchLoad,
   });
+
 
   // Fetching all courses
   const { data: courses, isSuccess: loadedCourses } = useQuery({
