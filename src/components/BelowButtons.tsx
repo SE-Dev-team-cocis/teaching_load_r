774ae5d3117044ed9ae603c5d806e07236d8c536
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-19 03:51:15 +0300

    Update BelowButtons.tsx
    Removed the use mutation hook

--- src/components/BelowButtons.tsx
+++ src/components/BelowButtons.tsx
@@ -1,12 +1,11 @@
-import useNewLoadStore21, { LecturerLoad } from "../zustand/newLoadStore2";
+import useNewLoadStore21 from "../zustand/newLoadStore2";
 import { useMutation, QueryClient } from "@tanstack/react-query";
 import { Lecturer, Load, assignLoad } from "../zustand/api/apis";
-import { toast } from "react-toastify";
 import CourseSubgroup, { Course } from "./load/CourseSubgroup";
 import useUserstore from "../zustand/userStore";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { memo, useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { successNotification } from "./utilities/toastify/Toastify";
 
 type AssignLoad = {
@@ -30,46 +29,31 @@ const BelowButtons = ({ broadcast }: ButtonProps) => {
   const semesterList = useNewLoadStore21((state) => state.semesterList);
   const lecturers = useNewLoadStore21((state) => state.lecturers);
 
-  const checkedLecturers = useNewLoadStore21((state) => state.checkedLecturers);
   const checkedCourses = useNewLoadStore21((state) => state.checkedCourses);
-  const checkedSemesterList = useNewLoadStore21(
-    (state) => state.checkedSemesterList
-  );
-
-  // console.log("Sem list: ", semesterList)
-
+  
   const setCheckedLecturers = useNewLoadStore21(
     (state) => state.setCheckedLecturers
   );
   const setCheckedCourses = useNewLoadStore21(
     (state) => state.setCheckedCourses
   );
-  const setCheckedSemesterList = useNewLoadStore21(
-    (state) => state.setCheckedSemesterList
-  );
+
   const setSemesterList = useNewLoadStore21((state) => state.setSemesterList);
   const setLecturers = useNewLoadStore21((state) => state.setLecturers);
-
-  const [theLoad, setTheLoad] = useState(lecturerLoad);
 
   const myCheckedCourse: Course = checkedCourses[0];
 
   const courseNames: string[] = [];
   const courseCreditUnits: number[] = [];
 
-  let lecturerID: number;
   const checkedLecturer = lecturers?.find((lect) =>
     lect.isChecked === true ? lect.id : null
   );
-  // console.log("Checked lecturer: ", checkedLecturer);
 
   const myid: any = checkedLecturer?.id;
+
+  // Function to assign courses
   const assignCourses = async () => {
-    // const lecturersIDs: number[] = checkedLecturers.map(
-    //   (lecturer) => lecturer.id
-    // );
-    // lecturerID = lecturersIDs[0];
-    // const lecturer = lecturers.find((lecturer) => lecturer.id === lecturerID);
 
     checkedCourses.forEach((course) => {
       courseNames.push(course.course_name);
@@ -85,30 +69,51 @@ const BelowButtons = ({ broadcast }: ButtonProps) => {
     };
 
 
-    // const url = "https://teaching-load-api.onrender.com/api/assign";
+    const url = "https://teaching-load-api.onrender.com/api/assign";
+    try{
+      
+  
+    const response = await axios.post(url, data, {
+      headers: {
+        "Content-Type": "application/json",
+        // "Authorization" : `Bearer ${localStorage.getItem('token')?JSON.parse(localStorage.getItem('token')):null}`
+      },
+    });
+    setLecturerLoad(response.data?.assignments?.assignments);
+    setCheckedLecturers([]);
+    setCheckedCourses([]);
+    setSemesterList(
+      semesterList.map((course: Course) => {
+        return { ...course, isChecked: false };
+      })
+    );
+    setLecturers(
+      lecturers.map((lecturer: Lecturer) => {
+        return { ...lecturer, isChecked: false };
+      })
+    );
 
-    // const response = await axios.post(url, data, {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     // "Authorization" : `Bearer ${localStorage.getItem('token')?JSON.parse(localStorage.getItem('token')):null}`
-    //   },
-    // });
+    successNotification(data.message); 
 
-    mutate(data); // call the mutation function which will update the assigned load table
+    }catch(error){
+      console.error(error)
+    }
+
+    // mutate(data); // call the mutation function which will update the assigned load table
   };
 
-  const { mutate, isLoading, isSuccess, data } = useMutation({
-    mutationFn: (data: AssignLoad) => assignLoad(data),
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["load"],
-      });
-      queryClient.refetchQueries({
-        queryKey: ["load"],
-        type: "active",
-      });
-    },
-  });
+  // const { mutate, isLoading, isSuccess, data } = useMutation({
+  //   mutationFn: (data: AssignLoad) => assignLoad(data),
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({
+  //       queryKey: ["load"],
+  //     });
+  //     queryClient.refetchQueries({
+  //       queryKey: ["load"],
+  //       type: "active",
+  //     });
+  //   },
+  // });
 
   let myLoad: any = [];
 
