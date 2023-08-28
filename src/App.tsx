017f89e279777b8d275fd2ed28a59c14ce34f392
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-28 04:59:01 +0300

    Update App.tsx
    Removed unnecessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import HomeAssign from "./components/HomeAssign";
 import Layout from "./components/Layout";
 import CentralDashboard from "./components/CentralDashboard";
 import SemesterCourses from "./components/load/SemesterCourses";
-import DepartmentDetails from "./components/load/DepartmentDetails";
-import Trial from "./components/Trial";
 import UnassignedCourses from "./components/load/UnassignedCourses";
 
 const App = () => {
@@ -20,12 +18,7 @@ const App = () => {
         <Route path="/teaching-load" element={<Home />} />
         <Route path="/teaching-load/new" element={<HomeAssign />} />
         <Route path="/teaching-load/central" element={<CentralDashboard />} />
-        {/* <Route
-          path="/teaching-load/central/:name"
-          element={<DepartmentDetails />}
-        /> */}
-
-        <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings />} />
         <Route path="/semestercourses" element={<SemesterCourses />} />
         <Route path="/trial" element={<UnassignedCourses id={1}/>} />
       </Route>
