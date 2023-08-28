4011d3b910f8d0d78274c9b4a236c4845279bf18
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-28 15:06:28 +0300

    Update AdminHome.tsx
    Added table for lecturers

--- src/components/backend/AdminHome.tsx
+++ src/components/backend/AdminHome.tsx
@@ -24,7 +24,65 @@ const AdminHome = () => {
     fetchLecturers();
   }, [])
   
-  return <div>Admin home page</div>;
+  return (
+  <div>
+    <p>Admin home page</p>
+        <table className="w-full border-2 border-b-gray-400 rounded">
+            <thead className="bg-gray-50 bottom-2 border-gray-200">
+              <tr>
+                <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+                  No.
+                </th>
+                <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+                  Name
+                </th>
+                <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-center">
+                 Role
+                </th>
+                <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-center">
+                 Department
+                </th>
+{/*                 <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-center">
+                  Action
+                </th> */}
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-400">
+              <tr>
+                <td>1</td>
+                <td>Loor Inho</td>
+                <td>Lecturer</td>
+                <td>Networks</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Pixy Dusty</td>
+                <td>Dean</td>
+                <td>Computer Science</td>
+              </tr>
+              
+{/*               {courseDetails.map((load, index) => (
+                <tr key={index}>
+                  <td className="p-2 text-sm text-gray-700 text-left">
+                    {index + 1}
+                  </td>
+                  <td className="p-2 text-sm text-gray-700 text-left">
+                    {load.course_name}
+                  </td>
+                  <td className="p-2 text-sm text-gray-700 text-center">
+                    {load.course_code}
+                  </td>
+                  <td className="p-2 text-sm text-gray-700 text-center">
+                    {load.course_cus}
+                  </td>
+                  <td className="p-2 text-sm text-gray-700 text-center">
+                    Delete
+                  </td>
+                </tr>
+              ))} */}
+            </tbody>
+          </table>
+  </div>);
 };
 
 export default AdminHome;
