0841a69bf71f5489dc806132576519e87b236d5f
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-31 21:09:05 +0300

    Update CentralDashboard.tsx
    Central dashboard cleanup

--- src/components/CentralDashboard.tsx
+++ src/components/CentralDashboard.tsx
@@ -12,45 +12,7 @@ const CentralDashboard = () => {
 
   const centralDashboard = useNewLoadStore21(state => state.centralDashboard)
   const [count, setCount] = useState(0);
-  // const [staff, setStaff] = useState([]);
-  // const [totalStaff, setTotalStaff] = useState([]);
-
-  // const [collegeLoad, setCollegeLoad] = useState({});
-  // const [departmentLoad, setDepartmentLoad] = useState([]);
-  // const [courseSummary, setCourseSummary] = useState({});
-
-  // const fetchSummary = async () => {
-  //   try {
-  //     const url = "https://teaching-load-api.onrender.com/api/dashboard";
-  //     const response = await axios.get(url);
-  //     // console.log(response.data)
-  //     const loadCount = response.data?.count;
-  //     if (loadCount === 0) {
-  //       setCount(loadCount);
-  //       return <p>{response.data.message}</p>;
-  //     }
-
-  //     // console.log("Central dashboard response: ", response.data);
-
-  //     setStaff(response.data?.staff);
-  //     setTotalStaff(response?.data?.total_staff);
-
-  //     setCollegeLoad(response?.data?.overall_total_load);
-  //     setDepartmentLoad(response?.data?.department_load);
-  //     setCourseSummary(response.data?.course_summary);
-
-  //     // console.log("Department load: ", departmentLoad)
-
-  //     // setCentralDashboard(response.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   // fetchCentralDashboardData();
-  //   // fetchSummary();
-  // }, []);
+  
   useEffect(()=>{
       fetchCentralDashboardData();
   }, [])
@@ -62,16 +24,9 @@ const CentralDashboard = () => {
         </p>
       ) : (
         <div>
-          {/* <TopCharts
-            collegeLoad={collegeLoad}
-            totalStaff={totalStaff}
-            courseSummary={courseSummary}
-          /> */}
           <TopCharts />
           <div className="grid grid-cols-12 gap-2 px-5">
-            {/* {departmentLoad?.map((department: any, index: number) => ( */}
             {centralDashboard.department_load?.map((department: any, index: number) => (
-
               <div key={index} className="col-span-3">
                 <Departments department={department} staff={centralDashboard.staff} />
               </div>
