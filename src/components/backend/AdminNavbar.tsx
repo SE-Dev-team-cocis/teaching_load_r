fc9ac78a97b06b8cdbffc6a139d0fcc7da69f16b
Loorinho <romeojacobson@gmail.com>
2023-10-21 16:08:53 +0300

    fetched colleges within the colleges component and displayed them

--- src/components/backend/AdminNavbar.tsx
+++ src/components/backend/AdminNavbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const AdminNavbar = () => {
   return (
-    <nav className='mt-5 ml-4'>
+    <nav className='mt-5 ml-4 admin-nav'>
         <ul className='flex gap-5'>
             <NavLink to={"/admin/staff"}><li>Staff</li></NavLink>
             <NavLink to={"/admin/colleges"}><li>Colleges</li></NavLink>
--- src/components/backend/Colleges.tsx
+++ src/components/backend/Colleges.tsx
@@ -1,7 +1,68 @@
+import { useMemo, useState } from "react";
+import { CollegeType } from "./functions/BackendSchemas";
+import axios from "axios";
 
 const Colleges = () => {
+
+    const url =
+    "https://teachingloadfive-82f4e24a-6a04-4f8b-8cae.cranecloud.io/api/college";
+
+  const [colleges, setColleges] = useState<CollegeType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchColleges = async () => {
+    try {
+      setLoading(true);
+      const response = await axios(url);
+      setColleges(response.data.colleges);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
+  useMemo(() => {
+    fetchColleges();
+  }, []);
   return (
-    <div>Colleges</div>
+    <section className="mt-4 px-4">
+         <table className="w-full border-2 border-b-gray-400 rounded">
+        <thead className="bg-gray-50 bottom-2 border-gray-200">
+          <tr>
+            <th className="w-10 p-2 text-sm font-semibold tracking-wide text-left">
+              No.
+            </th>
+            <th className=" w-20 p-2 text-sm font-semibold tracking-wide text-left">
+              Name
+            </th>
+            <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-left">
+                College Code 
+            </th>
+            {/*                 <th className=" w-10 p-2 text-sm font-semibold tracking-wide text-center">
+                  Action
+                </th> */}
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-400">
+          {colleges?.map((college: any, index) => (
+            <tr key={index} className="hover:bg-green-400 hover:bg-opacity-20">
+              <td className="p-2 text-sm text-gray-700 text-left">
+                {index + 1}
+              </td>
+              <td className="p-2 text-sm text-gray-700 text-left">
+                {college.college_name}
+              </td>
+           
+              <td className="p-2 text-sm text-gray-700 text-left">
+                {college.college_code}
+              </td>
+              {/* <td className="p-2 text-sm text-gray-700 text-center">
+                    Delete
+                  </td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   )
 }
 
